f9e0aaf51eafb6616f2647f55ca6ba20d3022a48
Osefx <cinci57450@gmail.com>
2022-05-24 10:49:06 +0200

    correction des erreurs

--- src/main/java/com/inti/SpringBoot_Groupe_Aeroport/service/VolServiceImpl.java
+++ src/main/java/com/inti/SpringBoot_Groupe_Aeroport/service/VolServiceImpl.java
@@ -4,44 +4,25 @@ import com.inti.SpringBoot_Groupe_Aeroport.repository.VolRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
-import com.inti.SpringBoot_Groupe_Aeroport.model.Vol;
-
-
-@Service
-public class VolServiceImpl implements VolService {
-    @Autowired
-    VolRepository volRepository;
-
-    @Override
-    public void save(Vol v){
-
-        volRepository.save(v);
-    }
-=======
 
 import java.util.List;
 
 import com.inti.SpringBoot_Groupe_Aeroport.model.Vol;
 
-
 @Service
 public class VolServiceImpl implements VolService {
-    @Autowired
-    VolRepository volRepository;
+	@Autowired
+	VolRepository volRepository;
 
-    @Override
-    public void save(Vol v){
+	@Override
+	public void save(Vol v) {
 
-        volRepository.save(v);
-    }
+		volRepository.save(v);
+	}
 
 	@Override
 	public List<Vol> getVols() {
 		return volRepository.findAll();
 	}
->>>>>>> branch 'master' of https://github.com/Osefx/SpringBoot_groupe_aeroport.git
-
-
 
 }
